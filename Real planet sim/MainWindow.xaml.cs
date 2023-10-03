e35f872634aa6860a09a0b46944e6bc4ea45c9ed
Rasmus <rasmustofthermansen@gmail.com>
2023-10-03 06:51:08 +0200

    Tilføjet flere billeder + justeret på afstand

--- Real planet sim/MainWindow.xaml.cs
+++ Real planet sim/MainWindow.xaml.cs
@@ -63,14 +63,14 @@ namespace Real_planet_sim
         public void CreatePlanets(string time)
         {
             mercury.StartPlanet(Merkur, sun, time, 47.87, 70,slider);
-            venus.StartPlanet(Venus, sun, time, 35.02, 105, slider);
-            earth.StartPlanet(Eeath, sun, time, 29.78, 135, slider);
-            mars.StartPlanet(Mars, sun, time, 24.077, 205, slider);
-            jupiter.StartPlanet(Jupiter, sun, time, 13.07, 240, slider);
-            saturn.StartPlanet(Saturn, sun, time, 9.69, 275, slider);
-            uranus.StartPlanet(Uranus, sun, time, 6.81, 310, slider);
-            neptune.StartPlanet(Neptune, sun, time, 5.43, 340, slider);
-            pluto.StartPlanet(Pluto, sun, time, 4.74, 380, slider);
+            venus.StartPlanet(Venus, sun, time, 35.02, 106, slider);
+            earth.StartPlanet(Eeath, sun, time, 29.78, 141, slider);
+            mars.StartPlanet(Mars, sun, time, 24.077, 176, slider);
+            jupiter.StartPlanet(Jupiter, sun, time, 13.07, 211, slider);
+            saturn.StartPlanet(Saturn, sun, time, 9.69, 246, slider);
+            uranus.StartPlanet(Uranus, sun, time, 6.81, 281, slider);
+            neptune.StartPlanet(Neptune, sun, time, 5.43, 316, slider);
+            pluto.StartPlanet(Pluto, sun, time, 4.74, 351, slider);
         }
         private void PositionWindowAtTopLeft()
         {
